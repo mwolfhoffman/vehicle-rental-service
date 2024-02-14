57ff3b8288e1990187b1029056778f9f8c9854fc
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-02-14 15:18:24 -0700

    fixed resource locations error

--- infra/main.tf
+++ infra/main.tf
@@ -15,30 +15,30 @@ resource "azurerm_resource_group" "vehicleservice_resource_group" {
 
 resource "azurerm_app_service_plan" "vehicleservice_app_service_plan" {
   name                = "vehicleservice-appservice-plan"
-  location            = azurerm_resource_group.vehicleservice_resource_group
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
   resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
   kind                = "Linux"
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
 resource "azurerm_app_service" "vehicleservice_app_service" {
   name                = "vehicleservice"
-  location            = azurerm_resource_group.vehicleservice_resource_group
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
   resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
   app_service_plan_id = azurerm_resource_group.vehicleservice_resource_group.id
 
   site_config {
-    linux_fx_version = "DOCKER|<DOCKER_IMAGE_NAME>"
+    linux_fx_version = "DOCKER>|" + docker_image
   }
 }
 
 resource "azurerm_cosmosdb_account" "vehicleservice_db" {
   name                = "vehicleservice-cosmosdb-account"
-  location            = azurerm_resource_group.vehicleservice_resource_group
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
   resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
@@ -54,5 +54,6 @@ resource "azurerm_cosmosdb_account" "vehicleservice_db" {
 }
 
 output "cosmosdb_connection_string" {
-  value = azurerm_cosmosdb_account.vehicleservice_db.connection_strings[0]
+  sensitive = true
+  value     = azurerm_cosmosdb_account.vehicleservice_db.connection_strings[0]
 }
