c07ac972463801b71221c4b1d85dc6a81311538c
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-02-15 08:37:25 -0700

    created registry

--- infra/main.tf
+++ infra/main.tf
@@ -12,35 +12,27 @@ resource "azurerm_resource_group" "vehicleservice_resource_group" {
   location = "West US"
 }
 
-resource "azurerm_app_service_plan" "vehicleservice_app_service_plan" {
+
+resource "azurerm_service_plan" "vehicleservice_resource_group" {
+  name                = "vehicleservice_resource_group"
+  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+
+resource "azurerm_linux_web_app" "vehicleservice_app_service" {
   name                = "vehicleservice-appservice-plan"
   location            = azurerm_resource_group.vehicleservice_resource_group.location
   resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
-  kind                = "Linux"
-  reserved            = true
+  service_plan_id     = azurerm_service_plan.vehicleservice_resource_group.id
 
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-}
-
-resource "random_uuid" "service_id" {
-}
-
-resource "azurerm_app_service" "vehicleservice_app_service" {
-  name                = "vehicleservice-${random_uuid.service_id.result}"
-  location            = azurerm_resource_group.vehicleservice_resource_group.location
-  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.vehicleservice_app_service_plan.id
-
-  site_config {
-    app_command_line = "docker build -t ${var.docker_image}:latest ../VehicleService"
-  }
+  site_config {}
 }
 
 resource "azurerm_cosmosdb_account" "vehicleservice_db" {
-  name                = "vehicleservice-cosmosdb-account-${random_uuid.service_id.result}"
+  name                = "vehicleservice-cosmos-db"
   location            = azurerm_resource_group.vehicleservice_resource_group.location
   resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
   offer_type          = "Standard"
@@ -59,4 +51,12 @@ resource "azurerm_cosmosdb_account" "vehicleservice_db" {
 output "cosmosdb_connection_string" {
   sensitive = true
   value     = azurerm_cosmosdb_account.vehicleservice_db.connection_strings[0]
+}
+
+resource "azurerm_container_registry" "container_registry" {
+  name                = "containerregistry54839583493"
+  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
